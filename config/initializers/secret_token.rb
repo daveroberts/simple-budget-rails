81d0f551ebd247421b234ba5c9f2e7ca84048c30
Dave Roberts <dave.a.roberts@gmail.com>
2012-02-19 19:49:18 -0500

    initial db action

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplebudget::Application.config.secret_token = 'ecafa07c550c3ee61e3d8c70592074e35c584f4a7a902f772d8bb314aed820e3cbee9463e356d395937834ceb6eae8b3a110ddde27fc6e3e0273d3d24d6a3ee2'
